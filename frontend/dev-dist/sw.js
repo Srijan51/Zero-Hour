c6c7d3b325472144258a38c2c11f8cf71f19203c
Srijan <dsrijan72@gmail.com>
2026-04-25 16:33:52 +0530

    Add psycopg2-binary and update SW revision
    Add psycopg2-binary to backend/requirements.txt to provide PostgreSQL client support. Update the service worker revision for index.html in frontend/dev-dist/sw.js (revision changed from "0.qa6f9msrh9g" to "0.abus6pg824s") to force cache invalidation and trigger a new service worker activation.

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.qa6f9msrh9g"
+    "revision": "0.abus6pg824s"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
